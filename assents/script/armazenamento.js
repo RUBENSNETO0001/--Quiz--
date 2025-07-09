01c079f246543d0c7041439d3eba2db4e4490e25
RUBENSNETO0001 <rubensnetomartinssuarezneto@gmail.com>
2025-07-09 15:42:42 -0300

    tentativa de fazer um sistema de login

--- assents/script/armazenamento.js
+++ assents/script/armazenamento.js
@@ -1,10 +1,25 @@
-function enviar(){
+function enviar() {
+    let i = 0;
     const nome = document.getElementById("nome").value;
-
     const usuario = new Map();
+    const pontos = 19;
     
-    for(let i of numeros)
-    usuario.set(nome,0);
+    for(let i = 0; i < localStorage.length; i++){
+        const chave = localStorage.getItem(`usuario${i}`);
+        if(true){
+            const usuarioExistente = JSON.parse(chave);
+            if (usuarioExistente.nome === nome) {
+                alert("Usuário já cadastrado, tente novamente.");
+                return;
+            }
+        }
+    }
 
-    console.log(usuario);
+    if (true) {
+        document.getElementById("form").style.display = "none";
+        document.getElementById("opcao_quiz").style.display = "block";
+    }
+
+    localStorage.setItem(`usuario${i}`, JSON.stringify(Array.from(usuario.entries())));
+    usuario.set({"nome": nome, "pontos": pontos});
 }
