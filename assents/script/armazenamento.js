0f5479c42d3d6cc8d87059c76fffdf71c726c755
RUBENSNETO0001 <rubensnetomartinssuarezneto@gmail.com>
2025-07-09 17:20:33 -0300

    atualização

--- assents/script/armazenamento.js
+++ assents/script/armazenamento.js
@@ -1,25 +1,14 @@
 function enviar() {
-    let i = 0;
     const nome = document.getElementById("nome").value;
-    const usuario = new Map();
-    const pontos = 19;
     
-    for(let i = 0; i < localStorage.length; i++){
-        const chave = localStorage.getItem(`usuario${i}`);
-        if(true){
-            const usuarioExistente = JSON.parse(chave);
-            if (usuarioExistente.nome === nome) {
-                alert("Usuário já cadastrado, tente novamente.");
-                return;
-            }
-        }
-    }
-
+    const usuario = new Map();
+    const pontos = 0;
+    
     if (true) {
-        document.getElementById("form").style.display = "none";
+        document.getElementById("login").style.display = "none";
         document.getElementById("opcao_quiz").style.display = "block";
     }
 
-    localStorage.setItem(`usuario${i}`, JSON.stringify(Array.from(usuario.entries())));
+    localStorage.setItem(`usuario`, JSON.stringify(Array.from(usuario.entries())));
     usuario.set({"nome": nome, "pontos": pontos});
 }
