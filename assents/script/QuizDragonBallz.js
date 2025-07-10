7048aa7109768bc81dc2c2fa1f89376741f3c252
RUBENSNETO0001 <RUBENSNETOMARTINSSUAREZNETO@GMAIL.COM>
2025-07-10 15:08:21 -0500

    atualização

--- assents/script/QuizDragonBallz.js
+++ assents/script/QuizDragonBallz.js
@@ -1,35 +1,46 @@
-function IniciarQuizDB(){
-    let nome = prompt("Digite seu nome ou nick:");
-    
-    document.getElementById("body_quizes").style.display = "none";
-    document.getElementById("quiz_db").style.display = "block";
+let pontos = 0;
 
-    const perguntas = (
-        {
-        pergunta: "Qual é o verdadeiro nome do Goku?",
-        opcoes: ["Vegeta", "Kakarotto", "Raditz", "Bardock"],
-        resposta: "Kakarotto"
-      },
-      {
-        pergunta: "Quantas Esferas do Dragão existem?",
-        opcoes: ["5", "6", "7", "8"],
-        resposta: "7"
-      }
-    )
+function IniciarQuizDB() {
+  pontos = 0;
+  let nome = prompt("Digite seu nome ou nick:");
+  const div_quiz = document.getElementById("quiz_db");
 
-    for(let i = 0; i < perguntas.length; i++){
-        let resposta = document.getElementById('btnResposta');
+  document.getElementById("body_quizes").style.display = "none";
+  document.getElementById("quiz_db").style.display = "block";
 
-       const perguntaAtual = perguntas[i];
-  
-  // Exibe a pergunta no HTML
-  document.getElementById('Pergunta').innerText = perguntaAtual.pergunta;
+  const perguntas = [
+    {
+      pergunta: "Qual é o verdadeiro nome do Goku?",
+      opcoes: ["Vegeta", "Kakarotto", "Raditz", "Bardock"],
+      resposta: "Kakarotto"
+    },
+    {
+      pergunta: "Quantas Esferas do Dragão existem?",
+      opcoes: ["5", "6", "7", "8"],
+      resposta: "7"
     }
+  ];
 
+  div_quiz.innerHTML = '';
+
+  let resposta = document.createElement("div");
+
+  for (let i = 0; i < perguntas.length; i++) {
+    resposta.innerHTML += `<div class='div_op'>
+          <h2>${perguntas[i].pergunta}</h2>
+          ${perguntas[i].opcoes.map(opcao => `<button onclick="verificarResposta('${opcao}', '${perguntas[i].resposta}')">${opcao}</button>`).join("")}
+      `;
+      div_quiz.appendChild(resposta);
+  }
 }
 
+function verificador(x, y) {
+  if (x === y) {
+    alert("Você Acertou!!");
+    pontos++;
+  }
+  else {
+    alert("Você Errou!!");
+  }
+}
 
-// const usuario = new Map();
-// const pontos = 0;
-
-// usuario.set({ "nome": nome, "pontos": pontos });
